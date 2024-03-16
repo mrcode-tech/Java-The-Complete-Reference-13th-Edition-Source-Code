aa346b34ce2975f3de2f54f1d7c4464a7b0917e3
Mehdi Nakhjiri <mehdi.njr@gmail.com>
2024-03-16 13:43:01 +0330

    chapter18

--- Chapter-18/src/appendDemo.java
+++ Chapter-18/src/appendDemo.java
@@ -1,11 +1,43 @@
+import java.util.Arrays;
+import java.util.Locale;
+
 // Demonstrate append().
 class appendDemo {
     public static void main(String[] args) {
+
+        StringBuffer buffer = new StringBuffer("Hello \uD83D\uDE00 World"); // StringBuffer containing the "smiling face" emoji
+
+        // Find the index of the character after the space character
+        int startIndex = 6; // Index of the space character after "Hello"
+        int offset = 2; // Number of code points to offset
+        int newIndex = buffer.offsetByCodePoints(startIndex, offset);
+
+        System.out.println("New Index: " + newIndex); // Output: 9 (Index of the "W" character)
+
+        ///////////////////////////
+
+        String str = "apple,banana,,cherry,";
+        String[] parts = str.split(",", -1);
+        System.out.println(Arrays.toString(parts)); // [apple, banana, , cherry, ]
+
+        String[] parts2 = str.split(",", 0);
+        System.out.println(Arrays.toString(parts2)); // [apple, banana, , cherry]
+
+        String[] parts3 = str.split(",", 2); // parts3.length
+        System.out.println(Arrays.toString(parts3)); // [apple, banana, , cherry]
+
+        String input = "Hello";
+        String inputUpper = input.transform(x -> x.toUpperCase());
+        System.out.println(inputUpper); // Output: "HELLO"
+
+
         String s;
         int a = 42;
-        StringBuffer sb = new StringBuffer(40);
+        StringBuffer sb = new StringBuffer(10);
 
-        s = sb.append("a = ").append(a).append("!").toString();
+        sb.append("asfasfasfasgfasgasgasgasgasgagagsssssssssssssssssssssssssssssssss12345678901234567");
+        sb.ensureCapacity(100);
+        s = sb.append(" a = ").append(a).append("!").toString();
         System.out.println(s);
     }
 }
